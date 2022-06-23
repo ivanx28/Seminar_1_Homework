11371b5d7a3f01f4183eee239d74968513034256
ivan <lopatin.ivan28@icloud.com>
2022-06-23 22:41:26 +0300

    add task 6

---
+++ task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, определяющая чётное число или нет
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2 == 0)
+{
+    Console.WriteLine("Введённое число является чётным");
+}
+else
+{
+    Console.WriteLine("Введённое число является нечётным");
+}
