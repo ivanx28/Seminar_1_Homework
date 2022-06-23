1de26843fb3630b3bceee61480fc9fc27dcdf0e5
ivan <lopatin.ivan28@icloud.com>
2022-06-23 22:43:30 +0300

    edit task 4

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите первое число");
+Console.WriteLine("Введите третье число");
 int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
 
