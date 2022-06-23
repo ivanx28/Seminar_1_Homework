144b3ad13bb2bf90f25ab44f85da6981cdddec9a
ivan <lopatin.ivan28@icloud.com>
2022-06-23 22:36:44 +0300

    add task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, показывающая максимальное число их трёх введенных
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (b > a) max = b;
+if (c > b) max = c;
+
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
