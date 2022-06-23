ac7fc0b4efc3ba68d642713b635a349b3f8acdf7
ivan <lopatin.ivan28@icloud.com>
2022-06-23 22:27:09 +0300

    add task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// программа: поиск большего и меньшего из двух введённых чисел
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(a);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(b);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(a); 
+}
