8c696a2cd233a6723638bafc5486192c8d1f3bdf
ivan <lopatin.ivan28@icloud.com>
2022-06-26 22:30:09 +0300

    add task 8

---
+++ task_8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, отображающая все чётные числа от 1 до введенного числа.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+
+while (x <= num)
+{
+    if (x %2 == 0)
+    {
+        Console.WriteLine(x);
+        x ++;   
+    }
+    else
+    {
+        x ++;
+    }
+}
